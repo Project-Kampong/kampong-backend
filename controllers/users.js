761b515681c29064da636ddf77d823dc6eb06893
Don Tay Xi Cheng <dontay0209@gmail.com>
2020-07-08 17:56:38 +0800

    Add error response, pending separation of various error types into respective error response code

--- controllers/users.js
+++ controllers/users.js
@@ -4,26 +4,39 @@ const { db } = require('../config/db');
 // @route   GET /api/users
 // @acess   Public
 exports.getUsers = async (req, res) => {
-  const rows = await db.query('SELECT * FROM users');
+  try {
+    const rows = await db.manyOrNone('SELECT * FROM users');
 
-  res.status(200).json({
-    success: true,
-    data: rows
-  });
+    res.status(200).json({
+      success: true,
+      data: rows
+    });
+  } catch (e) {
+    return res.status(400).json({
+      success: false,
+      error: e
+    });
+  }
 };
 
 // @desc    Get single user
 // @route   GET /api/users/:id
 // @access  Private/Admin
 exports.getUser = async (req, res) => {
-  const rows = await db.query(
-    `SELECT * FROM users WHERE id = ${req.params.id}`
-  );
-
-  res.status(200).json({
-    success: true,
-    data: rows
-  });
+  try {
+    const rows = await db.one(
+      `SELECT * FROM users WHERE id = ${req.params.id}`
+    );
+    res.status(200).json({
+      success: true,
+      data: rows
+    });
+  } catch (e) {
+    return res.status(400).json({
+      success: false,
+      error: e
+    });
+  }
 };
 
 // @desc    Create user
@@ -36,12 +49,18 @@ exports.createUser = async (req, res) => {
 
   const createUserQuery = `INSERT INTO USERS (name, email, password, role) VALUES ('${name}', '${email}', '${password}', '${role}') RETURNING *`;
 
-  const rows = await db.query(createUserQuery);
-
-  res.status(201).json({
-    success: true,
-    data: rows
-  });
+  try {
+    const rows = await db.one(createUserQuery);
+    res.status(201).json({
+      success: true,
+      data: rows
+    });
+  } catch (e) {
+    return res.status(400).json({
+      success: false,
+      error: e
+    });
+  }
 };
 
 // @desc    Update single user
@@ -67,24 +86,38 @@ exports.updateUser = async (req, res) => {
   updateUserQuery = updateUserQuery.replace(/,\s*$/, ' ');
   updateUserQuery += `WHERE id = ${req.params.id} RETURNING *`;
 
-  const rows = await db.query(updateUserQuery);
+  try {
+    const rows = await db.one(updateUserQuery);
 
-  res.status(200).json({
-    success: true,
-    data: rows
-  });
+    res.status(200).json({
+      success: true,
+      data: rows
+    });
+  } catch (e) {
+    return res.status(400).json({
+      success: false,
+      error: e
+    });
+  }
 };
 
 // @desc    Delete single user
 // @route   DELETE /api/users/:id
 // @access  Private/Admin
 exports.deleteUser = async (req, res) => {
-  const row = await db.query(
-    `DELETE FROM users WHERE id = ${req.params.id} RETURNING *`
-  );
+  try {
+    const row = await db.one(
+      `DELETE FROM users WHERE id = ${req.params.id} RETURNING *`
+    );
 
-  res.status(200).json({
-    success: true,
-    data: row
-  });
+    res.status(200).json({
+      success: true,
+      data: row
+    });
+  } catch (e) {
+    return res.status(400).json({
+      success: false,
+      error: e
+    });
+  }
 };
--- db/schema-test.sql
+++ db/schema-test.sql
@@ -22,11 +22,11 @@ CREATE TABLE Users (
     CONSTRAINT fk_user_role FOREIGN KEY (role) REFERENCES UserRoles (id)
 );
 
--- Data population segment
+-- Mock data population
 INSERT INTO UserRoles (role) VALUES ('User');
 INSERT INTO UserRoles (role) VALUES ('Admin');
 
-INSERT INTO Users (email, name, password, role) VALUES ('dontay0209@gmail.com', 'Don', '123456', '2');
+INSERT INTO Users (email, name, password, role) VALUES ('don@gmail.com', 'Don', '123456', '2');
 INSERT INTO Users (email, name, password, role) VALUES ('john@gmail.com', 'John', '123456', '1');
 INSERT INTO Users (email, name, password, role) VALUES ('aaron@gmail.com', 'Aaron', '123456', '1');
 INSERT INTO Users (email, name, password, role) VALUES ('ben@gmail.com', 'Ben', '123456', '1');
