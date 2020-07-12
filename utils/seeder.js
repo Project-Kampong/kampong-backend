9f5068ad591aae8923145b6a5508d5e24e3cbf26
Don Tay Xi Cheng <dontay0209@gmail.com>
2020-07-12 12:04:28 +0800

    Add db mock data population and script

--- utils/seeder.js
+++ utils/seeder.js
@@ -6,10 +6,22 @@ checkConn();
 
 // Create a QueryFile globally, once per file:
 const schema = generateSqlQueryFile('../db/schema.sql');
+const data = generateSqlQueryFile('../db/mock-data.sql');
 
 const createTables = async () => {
   try {
     await db.tx(async query => await query.manyOrNone(schema));
+    console.log(`Tables created...`.green.inverse);
+    process.exit();
+  } catch (err) {
+    console.error(err.toString().red);
+    process.exit();
+  }
+};
+
+const importData = async () => {
+  try {
+    await db.tx(async query => await query.manyOrNone(data));
     console.log(`Data Imported...`.green.inverse);
     process.exit();
   } catch (err) {
@@ -20,4 +32,7 @@ const createTables = async () => {
 
 if (process.argv[2] === '-i') {
   createTables();
+}
+if (process.argv[2] === '-d') {
+  importData();
 }
