a2ab1e4967248594adc59d4f15f4c90a37e9d1d1
Don Tay Xi Cheng <dontay0209@gmail.com>
2020-07-13 15:11:36 +0800

    Refactor select, sort, pagination, and limit for advancedResult, left filterto refactor

--- middleware/advancedResults.js
+++ middleware/advancedResults.js
@@ -1,5 +1,6 @@
 const asyncHandler = require('./async');
 const { db } = require('../config/db');
+const { cleanseData } = require('../utils/dbHelper');
 
 /**
  * Handles select, sort, page, limit and filter request queries.
@@ -8,76 +9,72 @@ const { db } = require('../config/db');
  * by commas, and in the order to be parsed as SQL query statements.
  * In current implementation, filter queries can only check for exact
  * matches, and each attribute can only be matched to max 1 value.
- * Wrapping string values with '' is optional when filtering via
- * string matches.
- * Currently, populate only supports natural join with model.
  * Sample request query. ?select=name,password&sort=name,user_role&page=2&limit=2&name='Ron'
- * @param model:String the table name to query
- * @param populate:String the table name to form a natural join with model
+ * @param {String} model name of SQL table to query
  */
-const advancedResults = (model, populate) =>
+const advancedResults = model =>
   asyncHandler(async (req, res, next) => {
-    let query = `SELECT `;
+    // console.log(req.query);
+    let { select, sort, page = 1, limit = 25 } = req.query;
+    select = select ? select.split(',') : '*';
+    sort = sort ? sort.split(',') : sort;
+    page = parseInt(page);
+    limit = parseInt(limit);
 
-    // Handle select queries
-    if (req.query.select) {
-      query += req.query.select + ' ';
-    } else {
-      query += `* `;
-    }
+    const format = {
+      select,
+      model,
+      sort,
+      page,
+      limit
+    };
 
-    // handle model:String
-    query += `FROM ${model} `;
+    // remove undefined values (ie. remove sort if no sort value specified)
+    cleanseData(format);
+    // console.log(format);
 
-    // Populate each entry with natural join
-    if (populate) {
-      query += `NATURAL JOIN ${populate} `;
-    }
+    let query = 'SELECT ${select:name} FROM ${model:name} ';
 
-    // Copy req.query, if any
-    const reqQuery = { ...req.query };
+    // // Copy req.query, if any
+    // const reqQuery = { ...req.query };
 
-    // Query fields to exclude from reqQuery
-    const removeFields = ['select', 'sort', 'page', 'limit'];
+    // // Query fields to exclude from reqQuery
+    // const removeFields = ['select', 'sort', 'page', 'limit'];
 
-    // Remove fields from reqQuery
-    removeFields.forEach(field => delete reqQuery[field]);
+    // // Remove fields from reqQuery
+    // removeFields.forEach(field => delete reqQuery[field]);
 
-    let filterQuery = ``;
+    let filterQuery = '';
 
-    if (Object.keys(reqQuery).length !== 0) {
-      filterQuery += `WHERE `;
-      for (let [key, value] of Object.entries(reqQuery)) {
-        if (!(value.startsWith("'") && value.endsWith("'"))) {
-          value = `'${value}'`;
-        }
-        filterQuery += `${key} = ${value} AND `;
-      }
-      filterQuery = filterQuery.slice(0, filterQuery.lastIndexOf('AND '));
-    }
+    // if (Object.keys(reqQuery).length !== 0) {
+    //   filterQuery += `WHERE `;
+    //   for (let [key, value] of Object.entries(reqQuery)) {
+    //     filterQuery += `${key} = ${value} AND `;
+    //   }
+    //   filterQuery = filterQuery.slice(0, filterQuery.lastIndexOf('AND '));
+    // }
 
     query += filterQuery;
 
     // Handle sort
-    if (req.query.sort) {
-      query += `ORDER BY ${req.query.sort} `;
+    if (format.sort) {
+      query += 'ORDER BY ${sort:name} ';
     }
-
-    // Pagination (default val: 1 page, 25 entries)
-    const page = parseInt(req.query.page, 10) || 1;
-    const limit = parseInt(req.query.limit, 10) || 25;
 
     // start and end entry number for a page
     const startIndex = (page - 1) * limit;
     const endIndex = page * limit;
 
+    format.startIndex = startIndex;
+
     const totalEntries = await db.one(
-      `SELECT COUNT(*) FROM ${model} ${filterQuery}`
+      'SELECT COUNT(*) FROM ${model:name} ${filterQuery:raw}',
+      { model, filterQuery }
     );
 
-    query += `OFFSET ${startIndex} LIMIT ${limit}`;
+    query += 'OFFSET ${startIndex} LIMIT ${limit}';
 
-    const results = await db.manyOrNone(query);
+    const results = await db.manyOrNone(query, format);
 
     // Pagination results
     const pagination = {};
