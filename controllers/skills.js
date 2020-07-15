b2ab5666cd488386365ac465814e70e218ed8387
Don Tay Xi Cheng <dontay0209@gmail.com>
2020-07-15 16:01:38 +0800

    Add skills routing. Pending integration with profiles and listings.

---
+++ controllers/skills.js
@@ -0,0 +1,117 @@
+const { db, parseSqlUpdateStmt } = require('../config/db');
+const asyncHandler = require('../middleware/async');
+const ErrorResponse = require('../utils/errorResponse');
+
+/**
+ * @desc    Get all skills
+ * @route   GET /api/skills
+ * @access  Public
+ */
+exports.getSkills = asyncHandler(async (req, res) => {
+  res.status(200).json(res.advancedResults);
+});
+
+/**
+ * @desc    Get single skill
+ * @route   GET /api/skills/:id
+ * @access  Public
+ */
+exports.getSkill = asyncHandler(async (req, res) => {
+  const rows = await db.one(
+    'SELECT * FROM skills WHERE skill_id = $1',
+    req.params.id
+  );
+  res.status(200).json({
+    success: true,
+    data: rows
+  });
+});
+
+/**
+ * @desc    Create skill
+ * @route   POST /api/skills
+ * @access  Admin
+ */
+exports.createSkill = asyncHandler(async (req, res) => {
+  const { skill } = req.body;
+
+  const data = {
+    skill
+  };
+
+  const rows = await db.one(
+    'INSERT INTO skills (${this:name}) VALUES (${this:csv}) RETURNING *',
+    data
+  );
+
+  res.status(201).json({
+    success: true,
+    data: rows
+  });
+});
+
+/**
+ * @desc    Update single skill
+ * @route   PUT /api/skills/:id
+ * @access  Admin
+ */
+exports.updateSkill = asyncHandler(async (req, res, next) => {
+  // check if skill exists
+  const skillExists = await db.oneOrNone(
+    'SELECT * FROM skills WHERE skill_id = $1',
+    req.params.id
+  );
+
+  // return bad request response if invalid skill
+  if (!skillExists) {
+    return next(new ErrorResponse(`Skill does not exist`, 400));
+  }
+
+  const { skill } = req.body;
+
+  const data = {
+    skill
+  };
+
+  const updateSkillQuery = parseSqlUpdateStmt(
+    data,
+    'skills',
+    'WHERE skill_id = $1 RETURNING *',
+    [req.params.id]
+  );
+
+  const rows = await db.one(updateSkillQuery);
+
+  res.status(200).json({
+    success: true,
+    data: rows
+  });
+});
+
+/**
+ * @desc    Delete single skill
+ * @route   DELETE /api/skills/:id
+ * @access  Admin
+ */
+exports.deleteSkill = asyncHandler(async (req, res, next) => {
+  // check if skill exists
+  const skill = await db.oneOrNone(
+    'SELECT * FROM skills WHERE skill_id = $1',
+    req.params.id
+  );
+
+  // return bad request response if invalid skill
+  if (!skill) {
+    return next(new ErrorResponse(`Skill does not exist`, 400));
+  }
+
+  const rows = await db.one(
+    'DELETE FROM skills WHERE skill_id = $1 RETURNING *',
+    req.params.id
+  );
+
+  res.status(200).json({
+    success: true,
+    data: rows
+  });
+});
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const listingRoute = require('./listing-routes');
+const skillRoute = require('./skill-routes');
 const userRoute = require('./user-routes');
 const authRoute = require('./auth-routes');
 
@@ -8,5 +9,6 @@ const authRoute = require('./auth-routes');
 router.use('/listings', listingRoute);
 router.use('/users', userRoute);
 router.use('/auth', authRoute);
+router.use('/skills', skillRoute);
 
 module.exports = router;
