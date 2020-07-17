bbba8fedcd51e764791abdd470cf74b0fbc4c0b6
Don Tay Xi Cheng <dontay0209@gmail.com>
2020-07-17 16:23:10 +0800

    Rename profile route params for consistency

--- routes/profile-routes.js
+++ routes/profile-routes.js
@@ -6,7 +6,7 @@ const skillRoute = require('./skill-routes');
 const participantRoute = require('./participant-routes');
 
 // Re-route this URI to other resource router
-router.use('/:profile_id/skills', skillRoute);
+router.use('/:user_id/skills', skillRoute);
 router.use('/:user_id/participants', participantRoute);
 
 module.exports = router;
