5daadd1a091039818bc74080f60214b857641d5b
Kalsyc <klassiclegend@gmail.com>
2020-07-18 14:53:40 +0800

    Add profile stuff

---
+++ controllers/profiles.js
@@ -0,0 +1,117 @@
+const { hashPassword } = require('../utils/auth.js');
+const { db, parseSqlUpdateStmt } = require('../config/db');
+const asyncHandler = require('../middleware/async');
+const ErrorResponse = require('../utils/errorResponse');
+const { cleanseData } = require('../utils/dbHelper');
+
+/**
+ * @desc    Get all profiles
+ * @route   GET /api/profiles
+ * @access  Public
+ */
+exports.getProfiles = asyncHandler(async (req, res) => {
+  res.status(200).json(res.advancedResults);
+});
+
+/**
+ * @desc    Get single profile
+ * @route   GET /api/profiles/:id
+ * @access  Public
+ */
+exports.getProfile = asyncHandler(async (req, res) => {
+  const rows = await db.one(
+    'SELECT * FROM profiles WHERE user_id = $1',
+    req.params.id
+  );
+  res.status(200).json({
+    success: true,
+    data: rows
+  });
+});
+
+/**
+ * @desc    Update single profile
+ * @route   PUT /api/profiles/:id
+ * @access  Private
+ */
+exports.updateProfile = asyncHandler(async (req, res, next) => {
+  // check if user exists
+  const isValidUser = await db.oneOrNone(
+    'SELECT * FROM profiles WHERE user_id = $1',
+    req.params.id
+  );
+
+  // return bad request response if invalid user
+  if (!isValidUser) {
+    return next(new ErrorResponse(`User does not exist`, 400));
+  }
+
+  const { profile_picture, about, interest, phone, facebook_link, twitter_link, instagram_link, linkedin_link } = req.body;
+
+  const data = {
+    profile_picture,
+    about,
+    interest,
+    phone,
+    facebook_link,
+    twitter_link,
+    instagram_link,
+    linkedin_link
+  };
+
+  cleanseData(data);
+
+  const updateProfileQuery = parseSqlUpdateStmt(
+    data,
+    'profiles',
+    'WHERE user_id = $1 RETURNING *',
+    [req.params.id]
+  );
+
+  const rows = await db.one(updateProfileQuery);
+
+  res.status(200).json({
+    success: true,
+    data: rows
+  });
+});
+
+/**
+ * @desc    Verify single profile
+ * @route   PUT /api/profiles/:id
+ * @access  Admin
+ */
+exports.verifyProfile = asyncHandler(async (req, res, next) => {
+  // check if user exists
+  const isValidUser = await db.oneOrNone(
+    'SELECT * FROM profiles WHERE user_id = $1',
+    req.params.id
+  );
+
+  // return bad request response if invalid user
+  if (!isValidUser) {
+    return next(new ErrorResponse(`User does not exist`, 400));
+  }
+
+  const { is_verified } = req.body;
+
+  const data = {
+    is_verified
+  };
+
+  cleanseData(data);
+
+  const updateIsVerifiedQuery = parseSqlUpdateStmt(
+    data,
+    'profiles',
+    'WHERE user_id = $1 RETURNING *',
+    [req.params.id]
+  );
+
+  const rows = await db.one(updateIsVerifiedQuery);
+
+  res.status(200).json({
+    success: true,
+    data: rows
+  });
+});
--- routes/profile-routes.js
+++ routes/profile-routes.js
@@ -1,4 +1,8 @@
 const express = require('express');
+const { check, oneOf } = require('express-validator');
+const advancedResults = require('../middleware/advancedResults');
+const { protect, authorise } = require('../middleware/auth');
+const { checkInputError } = require('../middleware/input-validation');
 const router = express.Router();
 
 // Include other resource's controllers to access their endpoints
@@ -6,5 +10,60 @@ const skillRoute = require('./skill-routes');
 
 // Re-route this URI to other resource router
 router.use('/:profileId/skills', skillRoute);
+
+// import controllers here
+const {
+    getProfiles,
+    getProfile,
+    updateProfile,
+    verifyProfile
+  } = require('../controllers/profiles');
+const { NO_FIELD_UPDATED_MSG } = require('../utils/inputExceptionMsg');
+  
+// map routes to controller
+router
+  .route('/')
+  .get(advancedResults('profiles'), getProfiles);
+
+router
+  .route('/:id')
+  .get(getProfile)
+  
+router.use(protect);
+
+router
+  .route('/:id')
+  .put(
+    [
+      oneOf(
+        [
+          check('profile_picture').exists(),
+          check('about').exists(),
+          check('interest').exists(),
+          check('phone').exists(),
+          check('facebook_link').exists(),
+          check('twitter_link').exists(),
+          check('instagram_link').exists(),
+          check('linkedin_link').exists(),
+          check('is_verified').exists()
+        ],
+        NO_FIELD_UPDATED_MSG
+      )
+    ],
+    checkInputError,
+    updateProfile,
+  )
+
+router.use(authorise('admin'));
+
+router
+    .route('/:id')
+    .put(
+      [
+        check('is_verified', NO_FIELD_UPDATED_MSG).exists()
+      ],
+    checkInputError,
+    verifyProfile
+    )
 
 module.exports = router;
