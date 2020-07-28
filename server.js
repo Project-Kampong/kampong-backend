edb59ad0c3e8b18a080ad044af8be2d79e4ab35f
Don Tay Xi Cheng <dontay0209@gmail.com>
2020-07-29 00:08:55 +0800

    Add api documentation as temporary landing page, for staging server

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
+const path = require('path');
 const express = require('express');
 const dotenv = require('dotenv');
 const colors = require('colors');
 const cookieParser = require('cookie-parser');
 const morgan = require('morgan');
+const helmet = require('helmet');
+const xss = require('xss-clean');
+const rateLimit = require('express-rate-limit');
+const hpp = require('hpp');
+const cors = require('cors');
 const { checkConn } = require('./config/db');
 const apiRoutes = require('./routes/api-routes');
 const errorHandler = require('./middleware/error');
@@ -24,6 +30,29 @@ app.use(cookieParser());
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+
+// Set security headers
+app.use(helmet());
+
+// Prevent XSS attacks
+app.use(xss());
+
+// Rate limiting
+const limiter = rateLimit({
+  windowMs: 5 * 60 * 1000, // 1 minutes
+  max: 40,
+});
+
+app.use(limiter);
+
+// Prevent http param pollution
+app.use(hpp());
+
+// Enable CORS
+app.use(cors());
+
+// Set static folder (ie. so all files can access public folder with './public/' and base directory of URI is public folder)
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Mount routers
 app.use('/api', apiRoutes);
