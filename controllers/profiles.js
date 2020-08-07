7c23ef629398e70fd908bbab228816c216ed7dd9
Don Tay Xi Cheng <dontay0209@gmail.com>
2020-08-08 02:47:16 +0800

    Fix bug with multiple file upload, refactor single file upload as well, to standardise on file upload flow

--- controllers/profiles.js
+++ controllers/profiles.js
@@ -162,7 +162,7 @@ exports.uploadPic = asyncHandler(async (req, res, next) => {
     );
   }
 
-  const profile_picture = req.file.location;
+  const { profile_picture } = req.body;
 
   const data = {
     profile_picture,
--- routes/test/upload-routes.js
+++ routes/test/upload-routes.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const router = express.Router();
 
+const { mapSingleFileLocation } = require('../../middleware/fileUploadHelper');
 const { uploadFile } = require('../../utils/fileUploader');
 
-router.post('/', uploadFile.single('file'), (req, res) => {
-  res.status(200).json({ success: true, data: req.file });
-});
+router.post(
+  '/',
+  uploadFile.single('file'),
+  mapSingleFileLocation('fileLocation'),
+  (req, res) => {
+    res.status(200).json({ success: true, data: req.body.fileLocation });
+  }
+);
 
 module.exports = router;
