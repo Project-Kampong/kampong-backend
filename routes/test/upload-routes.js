427ee7604b7b5c36761e34e6339dcdba519d3c9b
Don Tay Xi Cheng <dontay0209@gmail.com>
2020-08-12 20:41:39 +0800

    Minor refactor for test multi file upload endpoint

--- routes/test/upload-routes.js
+++ routes/test/upload-routes.js
@@ -13,7 +13,7 @@ router.post(
   }
 );
 
-router.post('/multi', uploadFile.array('file', 3), (req, res) => {
+router.post('/multi', uploadFile.array('files', 3), (req, res) => {
   res.status(200).json({ success: true, data: req.files });
 });
 
