d5072e12f49e675f0e3511bcd023e06569b9c4a9
Don Tay Xi Cheng <dontay0209@gmail.com>
2020-08-21 00:42:31 +0800

    Add frontend build to be served by express server. First integration of frontend and backend

--- server.js
+++ server.js
@@ -51,14 +51,22 @@ app.use(hpp());
 // Enable CORS
 app.use(cors());
 
-// Set static folder (ie. so all files can access public folder with './public/' and base directory of URI is public folder)
-app.use(express.static(path.join(__dirname, 'public')));
-
 // Mount routers
 app.use('/api', apiRoutes);
 
 // Mount error handler
 app.use(errorHandler);
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
+} else {
+    // use resources in public folder if in non-production mode, API documentation will be served as homepage
+    app.use(express.static(path.join(__dirname, 'public')));
+}
 
 const PORT = process.env.PORT || 5000;
 
