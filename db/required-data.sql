8f5d0318d5c4cf68152c9e1d7a854e9f31b4f1b3
Don Tay Xi Cheng <dontay0209@gmail.com>
2020-09-20 01:26:52 +0800

    Remove old locations data

--- db/required-data.sql
+++ db/required-data.sql
@@ -39,36 +39,6 @@ INSERT INTO "public"."skills" ("skill", "skill_group") VALUES
 ('Technical Documentation', 'Writing'),
 ('Requirements Gathering', 'Writing');
 
-/* Create default locations */
-INSERT INTO Locations (location) VALUES ('01 Raffles Place, Cecil, Marina, Peoples Park');
-INSERT INTO Locations (location) VALUES ('02 Anson, Tanjong Pagar');
-INSERT INTO Locations (location) VALUES ('03 Queenstown, Tiong Bahru');
-INSERT INTO Locations (location) VALUES ('04 Telok Blangah, Harbourfront');
-INSERT INTO Locations (location) VALUES ('05 Pasir Panjang, Hong Leong Garden, Clementi New Town');
-INSERT INTO Locations (location) VALUES ('06 High Street, Beach Road');
-INSERT INTO Locations (location) VALUES ('07 Middle Road, Golden Mile');
-INSERT INTO Locations (location) VALUES ('08 Little India');
-INSERT INTO Locations (location) VALUES ('09 Orchard, Cairnhill, River Valley');
-INSERT INTO Locations (location) VALUES ('10 Ardmore, Bukit Timah, Holland Road, Tanglin');
-INSERT INTO Locations (location) VALUES ('11 Watten Estate, Novena, Thomson');
-INSERT INTO Locations (location) VALUES ('12 Balestier, Toa Payoh, Serangoon');
-INSERT INTO Locations (location) VALUES ('13 Macpherson, Braddell');
-INSERT INTO Locations (location) VALUES ('14 Geylang, Eunos');
-INSERT INTO Locations (location) VALUES ('15 Katong, Joo Chiat, Amber Road');
-INSERT INTO Locations (location) VALUES ('16 Bedok, Upper East Coast, Eastwood, Kew Drive');
-INSERT INTO Locations (location) VALUES ('17 Loyang, Changi');
-INSERT INTO Locations (location) VALUES ('18 Tampines, Pasir Ris');
-INSERT INTO Locations (location) VALUES ('19 Serangoon Garden, Hougang, Ponggol');
-INSERT INTO Locations (location) VALUES ('20 Bishan, Ang Mo Kio');
-INSERT INTO Locations (location) VALUES ('21 Upper Bukit Timah, Clementi Park, Ulu Pandan');
-INSERT INTO Locations (location) VALUES ('22 Jurong');
-INSERT INTO Locations (location) VALUES ('23 Hillview, Dairy Farm, Bukit Panjang, Choa Chu Kang');
-INSERT INTO Locations (location) VALUES ('24 Lim Chu Kang, Tengah');
-INSERT INTO Locations (location) VALUES ('25 Kranji, Woodgrove');
-INSERT INTO Locations (location) VALUES ('26 Upper Thomson, Springleaf');
-INSERT INTO Locations (location) VALUES ('27 Yishun, Sembawang');
-INSERT INTO Locations (location) VALUES ('28 Seletar');
-
 INSERT INTO Locations (location) VALUES
 ('Admiralty'),
 ('Kranji'),
--- middleware/advancedResults.js
+++ middleware/advancedResults.js
@@ -22,115 +22,103 @@ const { cleanseData } = require('../utils');
  * @param {String} on column name common to both table to form an SQL join on (required if join is provided)
  */
 const advancedResults = (model, join, on) =>
-  asyncHandler(async (req, res, next) => {
-    let { select, sort, page = 1, limit = 25 } = req.query;
-    select = select ? select.split(',') : '*';
-    sort = sort ? sort.split(',') : sort;
-    page = parseInt(page);
-    limit = parseInt(limit);
+    asyncHandler(async (req, res, next) => {
+        let { select, sort, page = 1, limit = 25 } = req.query;
+        select = select ? select.split(',') : '*';
+        sort = sort ? sort.split(',') : sort;
+        page = parseInt(page);
+        limit = parseInt(limit);
 
-    const format = {
-      select,
-      model,
-      sort,
-      page,
-      limit,
-      join,
-      on,
-    };
-
-    // remove undefined values (eg. remove sort if no sort value specified)
-    cleanseData(format);
-
-    let query = pgp.as.format(
-      'SELECT ${select:name} FROM ${model:name} ',
-      format
-    );
-
-    if (join && on) {
-      query += pgp.as.format(
-        'JOIN ${join:raw} ON (${model:raw}.${on:raw} = ${join:raw}.${on:raw}) ',
-        format
-      );
-    }
-
-    // Copy req.query, if any
-    const reqQuery = { ...req.query };
-
-    // Query fields to exclude from reqQuery
-    const removeFields = ['select', 'sort', 'page', 'limit'];
-
-    // Remove fields from reqQuery
-    removeFields.forEach(field => delete reqQuery[field]);
-
-    // Handle WHERE clause of SQL statement
-    let filterQuery = '';
-
-    if (Object.keys(reqQuery).length !== 0) {
-      filterQuery += 'WHERE ';
-      for (let [key, value] of Object.entries(reqQuery)) {
-        const mappedFilter = {
-          key,
-          value: value.split(',').map(str => str.split(/'/).join('')), // remove all ' for pgp formatter to parse into sql
+        const format = {
+            select,
+            model,
+            sort,
+            page,
+            limit,
+            join,
+            on,
         };
 
-        filterQuery += pgp.as.format(
-          '${key:name} IN (${value:csv}) AND ',
-          mappedFilter
-        );
-      }
-      filterQuery = filterQuery.slice(0, filterQuery.lastIndexOf('AND '));
-    }
-    format.filterQuery = filterQuery;
+        // remove undefined values (eg. remove sort if no sort value specified)
+        cleanseData(format);
 
-    query += filterQuery;
+        let query = pgp.as.format('SELECT ${select:name} FROM ${model:name} ', format);
 
-    // Handle sort
-    if (format.sort) {
-      query += 'ORDER BY ${sort:name} ';
-    }
+        if (join && on) {
+            query += pgp.as.format('JOIN ${join:raw} ON (${model:raw}.${on:raw} = ${join:raw}.${on:raw}) ', format);
+        }
 
-    // start and end entry number for a page
-    const startIndex = (page - 1) * limit;
-    const endIndex = page * limit;
+        // Copy req.query, if any
+        const reqQuery = { ...req.query };
 
-    format.startIndex = startIndex;
+        // Query fields to exclude from reqQuery
+        const removeFields = ['select', 'sort', 'page', 'limit'];
 
-    const totalEntries = await db.one(
-      'SELECT COUNT(*) FROM ${model:name} ${filterQuery:raw}',
-      format
-    );
-    const count = parseInt(totalEntries.count);
+        // Remove fields from reqQuery
+        removeFields.forEach((field) => delete reqQuery[field]);
 
-    query += pgp.as.format('OFFSET ${startIndex} LIMIT ${limit}', format);
+        // Handle WHERE clause of SQL statement
+        let filterQuery = '';
 
-    let results = await db.manyOrNone(query, format);
+        if (Object.keys(reqQuery).length !== 0) {
+            filterQuery += 'WHERE ';
+            for (let [key, value] of Object.entries(reqQuery)) {
+                const mappedFilter = {
+                    key,
+                    value: value.split(',').map((str) => str.split(/'/).join('')), // remove all ' for pgp formatter to parse into sql
+                };
 
-    // Pagination results
-    const pagination = {};
+                filterQuery += pgp.as.format('${key:name} IN (${value:csv}) AND ', mappedFilter);
+            }
+            filterQuery = filterQuery.slice(0, filterQuery.lastIndexOf('AND '));
+        }
+        format.filterQuery = filterQuery;
 
-    if (endIndex < count) {
-      pagination.next = {
-        page: page + 1,
-        limit,
-      };
-    }
+        query += filterQuery;
 
-    if (startIndex > 0) {
-      pagination.prev = {
-        page: page - 1,
-        limit,
-      };
-    }
+        // Handle sort
+        if (format.sort) {
+            query += 'ORDER BY ${sort:name} ';
+        }
 
-    res.advancedResults = {
-      success: true,
-      count,
-      pagination,
-      data: results,
-    };
+        // start and end entry number for a page
+        const startIndex = (page - 1) * limit;
+        const endIndex = page * limit;
 
-    next();
-  });
+        format.startIndex = startIndex;
+
+        const totalEntries = await db.one('SELECT COUNT(*) FROM ${model:name} ${filterQuery:raw}', format);
+        const count = parseInt(totalEntries.count);
+
+        query += pgp.as.format('OFFSET ${startIndex} LIMIT ${limit}', format);
+
+        let results = await db.manyOrNone(query, format);
+
+        // Pagination results
+        const pagination = {};
+
+        if (endIndex < count) {
+            pagination.next = {
+                page: page + 1,
+                limit,
+            };
+        }
+
+        if (startIndex > 0) {
+            pagination.prev = {
+                page: page - 1,
+                limit,
+            };
+        }
+
+        res.advancedResults = {
+            success: true,
+            count,
+            pagination,
+            data: results,
+        };
+
+        next();
+    });
 
 module.exports = advancedResults;
