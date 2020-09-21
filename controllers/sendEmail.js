c8866b32b7fd1956eab49ddeb5d8c976fd39e160
abdurrahmanfaqihiskandar <e0052804@u.nus.edu>
2020-09-21 16:36:37 +0800

    Add a new controller sendEmail.js

---
+++ controllers/sendEmail.js
@@ -0,0 +1,22 @@
+const { sendEmail, ErrorResponse } = require('../utils');
+const { asyncHandler } = require('../middleware');
+
+exports.sendEmail = asyncHandler(async (req, res, next) => {
+    const { receiverEmail, senderEmail, subject, messsage } = req.body;
+
+    try {
+        await sendEmail({
+            email: receiverEmail,
+            cc: senderEmail,
+            subject,
+            messsage
+        });
+    } catch (err) {
+        return next(new ErrorResponse('Email cound not be sent. Please try again later.', 409));
+    }
+
+    res.status(200).json({
+        success: true,
+        data: {}
+    });
+});
