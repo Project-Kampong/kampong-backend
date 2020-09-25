7b83b5a333c599ec9d4c3d6624aa0dc0fc97532c
abdurrahmanfaqihiskandar <e0052804@u.nus.edu>
2020-09-25 21:18:04 +0800

    Remove try catch error handler

--- controllers/sendEmail.js
+++ controllers/sendEmail.js
@@ -4,17 +4,12 @@ const { asyncHandler } = require('../middleware');
 exports.sendEmail = asyncHandler(async (req, res, next) => {
     const { receiverEmail, senderEmail, subject, messsage } = req.body;
 
-    try {
-        await sendEmail({
-            email: receiverEmail,
-            cc: senderEmail,
-            subject,
-            messsage
-        });
-    } catch (err) {
-        //return next(new ErrorResponse('Email cound not be sent. Please try again later.', 409));
-        return next(err);
-    }
+    await sendEmail({
+        email: receiverEmail,
+        cc: senderEmail,
+        subject,
+        messsage
+    });
 
     res.status(200).json({
         success: true,
