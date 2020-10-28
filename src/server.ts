dd6851e47ab2591e84c592063d8ac2ea3faa9813
Don Tay Xi Cheng <don.tay@shopback.com>
2020-10-28 09:26:24 +0800

    Bump node to lts version and add diagnostic reporting

--- src/server.ts
+++ src/server.ts
@@ -72,15 +72,17 @@ app.get('*', (req, res) => {
     res.sendFile(pathToServe);
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = parseInt(process.env.PORT) || 5000;
 
 const server = app.listen(PORT, () => {
     console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold);
 });
 
 // Handle unhandled promise rejections
-process.on('unhandledRejection', (err, promise) => {
+process.on('unhandledRejection', (err: Error, promise: Promise<any>) => {
     console.log(`Unhandled Error: ${get(err, 'message')}`.bgRed);
+    // Write diagnostic report
+    process.report.writeReport(err);
     // Close server & exit process
     server.close(() => process.exit(1));
 });
