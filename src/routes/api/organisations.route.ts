a37300429c4e65cb9e34182ddea61ea2a0f0f1db
abdurrahmanfaqihiskandar <e0052804@u.nus.edu>
2020-11-10 19:45:06 +0800

    Update organisations api route

--- src/routes/api/organisations.route.ts
+++ src/routes/api/organisations.route.ts
@@ -30,8 +30,12 @@ const validateCreateOrganisationFields = [
     check('website_url', INVALID_URL_MSG).optional().trim().isURL(),
     check('phone', INVALID_PHONE_NUMBER_MSG).optional().trim().notEmpty(),
     check('email', INVALID_EMAIL_MSG).trim().notEmpty().isEmail().normalizeEmail(),
+    check('address', INVALID_ALPHA_SPACE_MSG('address')).optional().trim(),
     check('locations', INVALID_LOCATION_MSG).optional(),
     check('story', INVALID_ALPHA_SPACE_MSG('story')).optional().trim(),
+    check('facebook_link', INVALID_URL_MSG).optional().trim().isURL(),
+    check('twitter_link', INVALID_URL_MSG).optional().trim().isURL(),
+    check('instagram_link', INVALID_URL_MSG).optional().trim().isURL()
 ];
 
 const validateUpdateOrganisationFields = [
@@ -43,6 +47,12 @@ const validateUpdateOrganisationFields = [
             check('website_url').exists(),
             check('phone').exists(),
             check('email').exists(),
+            check('address').exists(),
+            check('locations').exists(),
+            check('story').exists(),
+            check('facebook_link').exists(),
+            check('twitter_link').exists(),
+            check('instagram_link').exists()
         ],
         NO_FIELD_UPDATED_MSG,
     ),
@@ -52,8 +62,12 @@ const validateUpdateOrganisationFields = [
     check('website_url', INVALID_URL_MSG).optional().trim().isURL(),
     check('phone', INVALID_PHONE_NUMBER_MSG).optional().trim().notEmpty(),
     check('email', INVALID_EMAIL_MSG).optional().trim().notEmpty().isEmail().normalizeEmail(),
+    check('address', INVALID_ALPHA_SPACE_MSG('address')).optional().trim(),
     check('locations', INVALID_LOCATION_MSG).optional(),
     check('story', INVALID_ALPHA_SPACE_MSG('story')).optional().trim(),
+    check('facebook_link', INVALID_URL_MSG).optional().trim().isURL(),
+    check('twitter_link', INVALID_URL_MSG).optional().trim().isURL(),
+    check('instagram_link', INVALID_URL_MSG).optional().trim().isURL()
 ];
 
 // Map public routes to controller
