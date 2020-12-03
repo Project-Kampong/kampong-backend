0c69b7fede66b79d35697d6095677955f16cf757
Faqih <e0052804@u.nus.edu>
2020-12-03 14:29:03 +0800

    Add organisation job repository

--- src/database/repositories/index.ts
+++ src/database/repositories/index.ts
@@ -2,6 +2,7 @@ import { FaqsRepository } from './faqs.repository';
 import { HashtagsRepository } from './hashtags.repository';
 import { JobsRepository } from './jobs.repository';
 import { ListingsRepository } from './listings.repository';
+import { OrganisationJobsRepository } from './organisationJobs.repository';
 
 // Database Interface Extensions (Add repositories here):
 interface IExtensions {
@@ -9,6 +10,7 @@ interface IExtensions {
     jobs: JobsRepository;
     listings: ListingsRepository;
     hashtags: HashtagsRepository;
+    organisationJobs: OrganisationJobsRepository;
 }
 
-export { IExtensions, FaqsRepository, HashtagsRepository, JobsRepository, ListingsRepository };
+export { IExtensions, FaqsRepository, HashtagsRepository, JobsRepository, ListingsRepository, OrganisationJobsRepository };
---
+++ src/database/repositories/organisationJobs.repository.ts
@@ -0,0 +1,36 @@
+import { IDatabase, IMain } from 'pg-promise';
+import { BaseRepository } from './base.repository';
+import { OrganisationJob, CreateOrganisationJobSchema, UpdateOrganisationJobSchema, DeactivateOrganisationJobSchema } from '../models';
+
+export class OrganisationJobsRepository extends BaseRepository {
+    constructor(protected readonly db: IDatabase<any>, protected readonly pgp: IMain) {
+        super(db, pgp);
+    }
+
+    getAllJobsForOrganisation(organisationId: string): Promise<OrganisationJob[]> {
+        return this.db.manyOrNone('SELECT * FROM organisationjob WHERE organisation_id = $1', organisationId);
+    }
+
+    getOrganisationJobById(organisationJobId: string): Promise<OrganisationJob> {
+        return this.db.one('SELECT * FROM organisationjob WHERE organisation_job_id = $1', organisationJobId);
+    }
+
+    createOrganisationJob(createOrganisationJobData: CreateOrganisationJobSchema): Promise<OrganisationJob> {
+        return this.db.one('INSERT INTO organisationjob (${this:name}) VALUES (${this:csv}) RETURNING *', createOrganisationJobData);
+    }
+
+    updateOrganisationJobById(updateOrganisationJobData: UpdateOrganisationJobSchema, organisationJobId: string): Promise<OrganisationJob> {
+        const updateOrganisationJobQuery = this.pgp.helpers.update(updateOrganisationJobData, null, 'organisationjob') + this.pgp.as.format(' WHERE organisation_job_id = $1 RETURNING *', organisationJobId);
+        return this.db.one(updateOrganisationJobQuery);
+    }
+
+    deactivateOrganisationJobById(deactivateOrganisationJobData: DeactivateOrganisationJobSchema, organisationJobId: string): Promise<OrganisationJob> {
+        const deactivateOrganisationJobQuery =
+            this.pgp.helpers.update(deactivateOrganisationJobData, null, 'organisationjob') + this.pgp.as.format(' WHERE organisation_job_id = $1 RETURNING *', organisationJobId);
+        return this.db.one(deactivateOrganisationJobQuery);
+    }
+
+    deleteOrganisationJobById(organisationJobId: string): Promise<OrganisationJob> {
+        return this.db.one('DELETE FROM organisationjob WHERE organisation_job_id = $1 RETURNING *', organisationJobId);
+    }
+}
