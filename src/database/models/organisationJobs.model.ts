151b597f50a541d5156aec777ebd01d076c38440
Faqih <e0052804@u.nus.edu>
2020-12-03 14:17:14 +0800

    Add orgnisation job model

---
+++ src/database/models/organisationJobs.model.ts
@@ -0,0 +1,22 @@
+export interface OrganisationJob {
+    organisation_job_id: number;
+    organisation_id: string;
+    organisation_job_title: string;
+    organisation_job_description: string;
+    deleted_on: Date;
+}
+
+export interface CreateOrganisationJobSchema {
+    organisation_id: string;
+    organisation_job_title: string;
+    organisation_job_description: string;
+}
+
+export interface UpdateOrganisationJobSchema {
+    organisation_job_title: string;
+    organisation_job_description: string;
+}
+
+export interface DeactivateOrganisationJobSchema {
+    deleted_on: Date;
+}
