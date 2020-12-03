175780c686459327d599b05ae62c5cf419450d58
Faqih <e0052804@u.nus.edu>
2020-12-04 02:07:01 +0800

    Add controller for getting single job

--- src/controllers/organisationJobs.ts
+++ src/controllers/organisationJobs.ts
@@ -33,6 +33,21 @@ export class OrganisationJobsController {
     });
 
     /**
+     * @desc    Get single organisation job
+     * @route   GET /api/organisation-jobs/:organisationJobId
+     * @access  Public
+     */
+    getSingleOrganisationJob = asyncHandler(async (req, res, next) => {
+        const organisationJobId = req.params.organisationJobId;
+
+        const job = await this.organisationJobsRepository.getOrganisationJobById(organisationJobId);
+        return res.status(200).json({
+            success: true,
+            data: job
+        });
+    });
+
+    /**
      * @desc    Create organisation job
      * @route   POST /api/organisation-jobs
      * @access  Owner/Admin
