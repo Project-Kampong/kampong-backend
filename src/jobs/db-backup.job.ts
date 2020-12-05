6280a2d63e6d8a381268822523ef21af31e10ab2
Don Tay Xi Cheng <don.tay@shopback.com>
2020-12-06 00:37:09 +0800

    Add db backup cronjob

---
+++ src/jobs/db-backup.job.ts
@@ -0,0 +1,32 @@
+import path from 'path';
+import moment from 'moment-timezone';
+import shell from 'shelljs';
+import { BaseJob } from './base.job';
+
+class DbBackupJob extends BaseJob {
+    constructor() {
+        super();
+    }
+
+    get frequency() {
+        return '* 0 5 * * *';
+    }
+
+    get command() {
+        return () => {
+            console.log('Running DB backup job'.bgYellow.grey.inverse.bold);
+            const backupDirectory = path.resolve(__dirname, '../../db-backup');
+            const backupFileName = `${process.env.PG_NAME}-${moment.tz(process.env.DEFAULT_TIMEZONE).format('YYYYMMDD-HHmmss')}`;
+            const backupFilePath = `${backupDirectory}/${backupFileName}`;
+            shell.exec(
+                `mkdir -p ${backupDirectory} && sudo -u ${process.env.PG_USER} pg_dump ${process.env.PG_NAME} > ${backupFilePath} --no-password`,
+                {
+                    fatal: true,
+                },
+            );
+            console.log(`DB backup job completed and stored at ${backupFilePath}`.bgGreen.grey.inverse.bold);
+        };
+    }
+}
+
+export const dbBackupJob = new DbBackupJob();
