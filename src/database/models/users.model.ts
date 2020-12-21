021deb6a9d68d175e8b57ebd1df4e3e1324a03d6
Faqih <e0052804@u.nus.edu>
2020-12-21 16:57:33 +0800

    Update user model

--- src/database/models/users.model.ts
+++ src/database/models/users.model.ts
@@ -22,7 +22,7 @@ export interface UpdateUserSchema {
     first_name: string,
     last_name: string,
     email: string,
-    password: string,
+    password?: string,
     role?: string
     is_activated?: boolean
 };
