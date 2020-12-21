e7948bf2a8a2b6ee82817b6e27e3937a2085a12e
Faqih <e0052804@u.nus.edu>
2020-12-22 01:43:13 +0800

    Move logic from passport configuration to controllers

--- src/utils/passport.ts
+++ src/utils/passport.ts
@@ -1,7 +1,8 @@
 import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
 import { Strategy as FacebookStrategy } from 'passport-facebook';
+// import { v1 as uuidv1 } from 'uuid';
 
-import { ErrorResponse } from './errorResponse';
+// import { ErrorResponse } from './errorResponse';
 import { db } from '../database';
 
 export const googlePassport = passport => {
@@ -11,19 +12,31 @@ export const googlePassport = passport => {
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
             callbackURL: '/auth/google-login/callback'
         },
-            async (accessToken, refreshToken, profile, done) => {
-                try {
-                    const user = await db.oneOrNone('SELECT * FROM loginuser WHERE email = $1', profile.emails[0].value);
-                    if (user) {
-                        if (user.google_id === null) {
-                            user.google_id = profile.id;
-                        }
-                        done(null, user);
-                    }
-                } catch (e) {
-                    const error = new ErrorResponse(e, e.statusCode);
-                    console.error(error);
-                }
+            (accessToken, refreshToken, profile, done) => {
+                // try {
+                //     const user = await db.oneOrNone('SELECT * FROM loginuser WHERE email = $1', profile.emails[0].value);
+                //     if (user) {
+                //         if (user.google_id === null) {
+                //             user.google_id = profile.id;
+                //         }
+                //         done(null, user);
+                //     }
+                //     if (!user) {
+                //         const userData = {
+                //             user_id: uuidv1(),
+                //             first_name: profile.name.givenName,
+                //             last_name: profile.name.familyName,
+                //             email: profile.emails[0].value,
+                //             google_id: profile.id
+                //         }
+                //         const newUser = db.one('INSERT INTO loginuser (${this:name}) VALUES (${this:csv}) RETURNING *', userData);
+                //         done(null, newUser);
+                //     }
+                // } catch (e) {
+                //     const error = new ErrorResponse(e, e.statusCode);
+                //     console.error(error);
+                // }
+                done(null, profile);
             })
     );
 
@@ -45,19 +58,31 @@ export const facebookPassport = passport => {
             clientSecret: process.env.FACEBOOK_APP_SECRET,
             callbackURL: '/auth/facebook-login/callback'
         },
-            async (accessToken, refreshToken, profile, done) => {
-                try {
-                    const user = await db.oneOrNone('SELECT * FROM loginuser WHERE email = $1', profile.emails[0].value);
-                    if (user) {
-                        if (user.facebook_id === null) {
-                            user.facebook_id = profile.id;
-                        }
-                        done(null, user);
-                    }
-                } catch (e) {
-                    const error = new ErrorResponse(e, e.statusCode);
-                    console.error(error);
-                }
+            (accessToken, refreshToken, profile, done) => {
+                // try {
+                //     const user = await db.oneOrNone('SELECT * FROM loginuser WHERE email = $1', profile.emails[0].value);
+                //     if (user) {
+                //         if (user.facebook_id === null) {
+                //             user.facebook_id = profile.id;
+                //         }
+                //         done(null, user);
+                //     }
+                //     if (!user) {
+                //         const userData = {
+                //             user_id: uuidv1(),
+                //             first_name: profile.name.givenName,
+                //             last_name: profile.name.familyName,
+                //             email: profile.emails[0].value,
+                //             facebook_id: profile.id
+                //         }
+                //         const newUser = db.one('INSERT INTO loginuser (${this:name}) VALUES (${this:csv}) RETURNING *', userData);
+                //         done(null, newUser);
+                //     }
+                // } catch (e) {
+                //     const error = new ErrorResponse(e, e.statusCode);
+                //     console.error(error);
+                // }
+                done(null, profile);
             })
     );
 
