6682e23d6644c1c05428f953ba76589a3eef6102
Faqih <e0052804@u.nus.edu>
2020-12-21 16:49:58 +0800

    Create user model

---
+++ src/database/models/users.model.ts
@@ -0,0 +1,28 @@
+export interface User {
+    user_id: string,
+    first_name: string,
+    last_name: string,
+    email: string,
+    password: string,
+    role: string
+    is_activated: boolean
+};
+
+export interface CreateUserSchema {
+    user_id: string,
+    first_name: string,
+    last_name: string,
+    email: string,
+    password: string,
+    role?: string
+    is_activated?: boolean
+};
+
+export interface UpdateUserSchema {
+    first_name: string,
+    last_name: string,
+    email: string,
+    password: string,
+    role?: string
+    is_activated?: boolean
+};
