8814bfc14356c3c99ea8e8a813be1e49a4b76fd7
Don Tay Xi Cheng <don.tay@shopback.com>
2021-01-04 17:00:34 +0800

    Add validation of query params for delete listingorg endpoint

--- src/routes/api/listingsOrganisations.route.ts
+++ src/routes/api/listingsOrganisations.route.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 export const router = express.Router();
-import { check } from 'express-validator';
+import { check, query } from 'express-validator';
 import { protect, authorise, checkInputError, asyncHandler } from '../../middleware';
 import { INVALID_FIELD_MSG } from '../../utils';
 
@@ -12,9 +12,13 @@ const validateCreateListingOrganisationEntryFields = [
     check('listing_id', INVALID_FIELD_MSG('listing id')).isUUID(),
     check('organisation_id', INVALID_FIELD_MSG('organisation id')).isUUID(),
 ];
+const validateDeleteListingOrganisationEntryFields = [
+    query('listingId', INVALID_FIELD_MSG('listing id')).isUUID(),
+    query('organisationId', INVALID_FIELD_MSG('organisation id')).isUUID(),
+];
 
 // Map routes to controller
 router
     .route('/')
     .post(protect, validateCreateListingOrganisationEntryFields, checkInputError, asyncHandler(createListingOrganisation))
-    .delete(protect, authorise('admin', 'owner'), asyncHandler(deleteListingOrganisation));
+    .delete(protect, validateDeleteListingOrganisationEntryFields, checkInputError, asyncHandler(deleteListingOrganisation));
