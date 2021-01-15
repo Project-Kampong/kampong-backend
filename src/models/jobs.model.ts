47dd0b83418691e6865b645331b70245f96ab2b1
Don Tay Xi Cheng <don.tay@shopback.com>
2021-01-15 13:44:47 +0800

    Refactor jobs with new validation format

--- src/models/jobs.model.ts
+++ src/models/jobs.model.ts
@@ -1,3 +1,6 @@
+import { IsNotEmpty, IsOptional, IsString, IsUUID } from 'class-validator';
+import { INVALID_FIELD_MSG } from '../utils';
+
 export interface Job {
     job_id: number;
     listing_id: string;
@@ -19,4 +22,27 @@ export interface UpdateJobSchema {
 
 export interface DeactivateJobSchema {
     deleted_on: Date;
+}
+
+export class CreateJobReqDto {
+    @IsUUID(undefined, { message: INVALID_FIELD_MSG('listing id ') })
+    listing_id: string;
+
+    @IsString({ message: INVALID_FIELD_MSG('job title') })
+    @IsNotEmpty({ message: INVALID_FIELD_MSG('job title') })
+    job_title: string;
+
+    @IsOptional()
+    @IsString({ message: INVALID_FIELD_MSG('job description') })
+    job_description: string;
+}
+
+export class UpdateJobReqDto {
+    @IsString({ message: INVALID_FIELD_MSG('job title') })
+    @IsNotEmpty({ message: INVALID_FIELD_MSG('job title') })
+    job_title: string;
+
+    @IsOptional()
+    @IsString({ message: INVALID_FIELD_MSG('job description') })
+    job_description: string;
 }
