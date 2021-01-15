984c69557bb64e698a0e6accefff411608bdc3b2
Don Tay Xi Cheng <don.tay@shopback.com>
2021-01-15 15:46:46 +0800

    Add async to all repository methods

--- src/database/repositories/jobs.repository.ts
+++ src/database/repositories/jobs.repository.ts
@@ -2,30 +2,30 @@ import { BaseRepository } from './base.repository';
 import { CreateJobSchema, DeactivateJobSchema, Job, UpdateJobSchema } from '../../models';
 
 export class JobsRepository extends BaseRepository {
-    getAllJobsForListing(listingId: string): Promise<Job[]> {
+    async getAllJobsForListing(listingId: string): Promise<Job[]> {
         return this.db.manyOrNone('SELECT * FROM job WHERE listing_id = $1', listingId);
     }
 
-    getJobById(jobId: string): Promise<Job> {
+    async getJobById(jobId: string): Promise<Job> {
         return this.db.one('SELECT * FROM job WHERE job_id = $1', jobId);
     }
 
-    createJob(createJobData: CreateJobSchema): Promise<Job> {
+    async createJob(createJobData: CreateJobSchema): Promise<Job> {
         return this.db.one('INSERT INTO job (${this:name}) VALUES (${this:csv}) RETURNING *', createJobData);
     }
 
-    updateJobById(updateJobData: UpdateJobSchema, jobId: string): Promise<Job> {
+    async updateJobById(updateJobData: UpdateJobSchema, jobId: string): Promise<Job> {
         const updateJobQuery = this.pgp.helpers.update(updateJobData, null, 'job') + this.pgp.as.format(' WHERE job_id = $1 RETURNING *', jobId);
         return this.db.one(updateJobQuery);
     }
 
-    deactivateJobById(deactivateJobData: DeactivateJobSchema, jobId: string): Promise<Job> {
+    async deactivateJobById(deactivateJobData: DeactivateJobSchema, jobId: string): Promise<Job> {
         const deactivateJobQuery =
             this.pgp.helpers.update(deactivateJobData, null, 'job') + this.pgp.as.format(' WHERE job_id = $1 RETURNING *', jobId);
         return this.db.one(deactivateJobQuery);
     }
 
-    deleteJobById(jobId: string): Promise<Job> {
+    async deleteJobById(jobId: string): Promise<Job> {
         return this.db.one('DELETE FROM job WHERE job_id = $1 RETURNING *', jobId);
     }
 }
