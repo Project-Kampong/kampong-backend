574fc854d9c3e8cf4114fd743ad021172e999fbb
Don Tay Xi Cheng <don.tay@shopback.com>
2021-02-20 17:09:55 +0800

    Add update user last seen db query

--- src/database/repositories/chatRooms.repository.ts
+++ src/database/repositories/chatRooms.repository.ts
@@ -10,7 +10,7 @@ export class ChatRoomsRepository extends BaseRepository {
     }
 
     getChatRoomAndMessagesById(ChatRoomId: string): Promise<ChatRoomMessage> {
-        return this.db.one('SELECT * FROM chatroom WHERE ChatRoom_id = $1', ChatRoomId);
+        return this.db.one('SELECT * FROM chatroom WHERE chatroom_id = $1', ChatRoomId);
     }
 
     createChatRoom(createChatRoomData: CreateChatRoomRequestDto): Promise<ChatRoom> {
@@ -19,5 +19,13 @@ export class ChatRoomsRepository extends BaseRepository {
 
     sendMessageToChatroom(sendMessageData: SendMessageRequestDto): Promise<ChatRoom> {
         return this.db.one('INSERT INTO chatmessage (${this:name}) VALUES (${this:csv}) RETURNING *', sendMessageData);
+    }
+
+    updateUserLastSeen(userChatRoomData: { user_id: string; chatroom_id: string }): Promise<{ last_seen: Date }> {
+        const { user_id, chatroom_id } = userChatRoomData;
+        return this.db.one('UPDATE chatparticipant SET last_seen = NOW() WHERE user_id = $1 AND chatroom_id = $2 RETURNING last_seen', [
+            user_id,
+            chatroom_id,
+        ]);
     }
 }
