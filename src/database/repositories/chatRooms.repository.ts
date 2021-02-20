6af405868dc607fd87544526665ea6f27b94f83d
Don Tay Xi Cheng <don.tay@shopback.com>
2021-02-20 16:35:12 +0800

    Update chatroom schema and repository methods

--- src/database/repositories/chatRooms.repository.ts
+++ src/database/repositories/chatRooms.repository.ts
@@ -1,19 +1,23 @@
 import { BaseRepository } from './base.repository';
-import { ChatRoom, ChatroomForUserResponseDto, CreateChatRoomSchema } from '../../models';
+import { ChatRoom, ChatRoomMessage, ChatRoomForUserResponseDto, CreateChatRoomRequestDto, SendMessageRequestDto } from '../../models';
 
 export class ChatRoomsRepository extends BaseRepository {
-    getAllChatRoomsForUser(userId: string): Promise<ChatroomForUserResponseDto[]> {
+    getAllChatRoomsForUser(userId: string): Promise<ChatRoomForUserResponseDto[]> {
         return this.db.manyOrNone(
             'SELECT cp.last_seen, cp.joined_on, cr.* FROM chatparticipant cp JOIN chatroom cr ON cp.chatroom_id = cr.chatroom_id WHERE cp.user_id = $1',
             userId,
         );
     }
 
-    getChatRoomById(ChatRoomId: string): Promise<ChatRoom> {
+    getChatRoomAndMessagesById(ChatRoomId: string): Promise<ChatRoomMessage> {
         return this.db.one('SELECT * FROM chatroom WHERE ChatRoom_id = $1', ChatRoomId);
     }
 
-    createChatRoom(createChatRoomData: CreateChatRoomSchema): Promise<ChatRoom> {
+    createChatRoom(createChatRoomData: CreateChatRoomRequestDto): Promise<ChatRoom> {
         return this.db.one('INSERT INTO chatroom (${this:name}) VALUES (${this:csv}) RETURNING *', createChatRoomData);
+    }
+
+    sendMessageToChatroom(sendMessageData: SendMessageRequestDto): Promise<ChatRoom> {
+        return this.db.one('INSERT INTO chatmessage (${this:name}) VALUES (${this:csv}) RETURNING *', sendMessageData);
     }
 }
--- src/models/chatRooms.model.ts
+++ src/models/chatRooms.model.ts
@@ -8,7 +8,21 @@ export interface ChatRoom {
     updated_on: Date;
 }
 
-export interface ChatroomForUserResponseDto {
+export interface ChatRoomMessage {
+    chatmessage_id: number;
+    user_id: string;
+    chatmessage_text: string;
+    reply_to: number;
+    file_links: string[];
+    created_on: Date;
+    updated_on: Date;
+}
+export interface ChatRoomMessageResponseDto {
+    users: { user_id: string; nickname: string; profile_picture: string }[];
+    messages: ChatRoomMessage[];
+}
+
+export interface ChatRoomForUserResponseDto {
     chatroom_id: string;
     chatroom_name: string;
     chatroom_pic: string;
@@ -20,9 +34,17 @@ export interface ChatroomForUserResponseDto {
     joined_on: Date;
 }
 
-export interface CreateChatRoomSchema {
+export interface CreateChatRoomRequestDto {
     chatroom_name: string;
     chatroom_pic: string;
     is_dm: boolean;
     user_ids: string[];
+}
+
+export interface SendMessageRequestDto {
+    chatroom_id: string;
+    user_id: string;
+    chatmessage_text: string;
+    reply_to: number;
+    file_links: string[];
 }
